bd44ceb0735741ea88732a7d5d83b2a433c75e27
jabbaum <jabbaum@gmail.com>
2023-12-07 16:45:15 -0800

    added the game overarching functionality

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let scoreCard = {
+    player: 0,
+    computer: 0
+};
 function getComputerChoice() {
     let num = Math.floor(
         (Math.random()*100)
@@ -14,14 +18,28 @@ function getComputerChoice() {
 }
 
 function singleround(pChoice = window.prompt("choose Rock, Paper, or Scissors!"), cChoice = getComputerChoice()) {
-    let playerChoice = pChoice.toLowerCase();
-    let computerChoice = cChoice.toLowerCase();
+    const playerChoice = pChoice.toLowerCase();
+    const computerChoice = cChoice.toLowerCase();
+
     let winnerOutput = `${playerChoice} beats ${computerChoice}!`
     if (playerChoice === computerChoice) {
         return `Both you and the computer chose ${computerChoice} so, the round is a tie. ${singleround()}`
     } else if(playerChoice ==='paper' && computerChoice === 'rock' || playerChoice ==='rock' && computerChoice === 'scissors' || playerChoice ==='scissors' && computerChoice === 'paper') {
+        scoreCard.player = scoreCard.player +1;
         return `You Win! ` + winnerOutput; 
     } else {
+        scoreCard.computer = scoreCard.computer +1;
         return `Sorry, you lose because the computer chose ${computerChoice} and that beats ${playerChoice}!`;
+    }
+}
+
+function game() {
+    for(let i=0; i<5; i++) {
+        singleround();
+        if(scoreCard.player>=3) {
+            return `The game is over! You Win!`
+        } else if (scoreCard.computer>=3) {
+            return `The game is over! You Lose! The computer wins!`
+        }
     }
 }
