148c1c45177b0de8c7d5cf3c0f946d6f8af13200
jabbaum <jabbaum@gmail.com>
2023-12-07 16:53:20 -0800

    Added a score log to keep updated on the current leader

--- script.js
+++ script.js
@@ -23,12 +23,15 @@ function singleround(pChoice = window.prompt("choose Rock, Paper, or Scissors!")
 
     let winnerOutput = `${playerChoice} beats ${computerChoice}!`
     if (playerChoice === computerChoice) {
+        console.log(`The current score is: Player: ${scoreCard.player} Computer: ${scoreCard.computer}`);
         return `Both you and the computer chose ${computerChoice} so, the round is a tie. ${singleround()}`
     } else if(playerChoice ==='paper' && computerChoice === 'rock' || playerChoice ==='rock' && computerChoice === 'scissors' || playerChoice ==='scissors' && computerChoice === 'paper') {
         scoreCard.player = scoreCard.player +1;
+        console.log(`The current score is: Player: ${scoreCard.player} Computer: ${scoreCard.computer}`);
         return `You Win! ` + winnerOutput; 
     } else {
         scoreCard.computer = scoreCard.computer +1;
+        console.log(`The current score is: Player: ${scoreCard.player} Computer: ${scoreCard.computer}`);
         return `Sorry, you lose because the computer chose ${computerChoice} and that beats ${playerChoice}!`;
     }
 }
